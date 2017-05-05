446eee219c7d58d8e74d7931baa3cbd97b89ffe5
anderwilson <ander.wilson@colostate.edu>
2017-05-05 10:30:26 -0600

    take family into bdlimglmall. fix bug with G=NULL.  add comments. replace post function with family.  Correct column names for DIC to avoid error.

--- R/bdlimglmall.R
+++ R/bdlimglmall.R
@@ -12,7 +12,7 @@
 #' @param nburn Number of MCMC iterations to be discarded as burning
 #' @param nthin Number of draws taken to obtain one sample
 #' @param prior List with the entries:  betavar = the prior variance for beta; and gamma = the prior variance for the covarites. The priors on beta and gamma are iid normal mean zero.
-#' @param post Relating to the posterior likelihood
+#' @param family Family as a funciton for glm
 #' @importFrom utils txtProgressBar setTxtProgressBar
 #' @author Ander Wilson
 #'
@@ -20,8 +20,8 @@
 
 
 
-
-bdlimglmall <- function(Y,X,Z,G,B,model,niter,nburn,nthin,prior,post){
+# Y=Y;X=x;Z=z1;G=drop(G);B=B;model=substring(Gmodel,7,15);niter=niter;nburn=nburn;nthin=nthin;prior=prior;family=family
+bdlimglmall <- function(Y,X,Z,G,B,model,niter,nburn,nthin,prior,family=family){
 
   #usefull stuff
   px = ncol(X)
@@ -56,30 +56,30 @@ bdlimglmall <- function(Y,X,Z,G,B,model,niter,nburn,nthin,prior,post){
   }else{
     ngb <- 1
     ngw <- 1
-    Gb <- rep(1,length(G))
-    Gw <- rep(1,length(G))
+    Gb <- rep(1,n)
+    Gw <- rep(1,n)
     ugw <- 1
     ugb <- 1
   }
   ng <- max(ngb,ngw)
 
   #starting values
-  theta <- unname(lm(rep(1,nrow(B$psi))~B$psi-1)$coef)
-  theta <- theta*sqrt(nrow(B$psi)/sum(theta^2))
-  theta <- rep(theta,ngw)
-  Xtheta <- matrix(0,n,ngb)
-  for(g in 1:ng) Xtheta[Gb==g,ugb[g]] <- X[Gb==g,] %*% theta[(ugw[g]-1)*px+1:px]
-  glmfit <- glm(Y~Xtheta+Z-1)
-  kappa <- glmfit$coef[1:ngb]
-  gamma <- glmfit$coef[-c(1:ngb)]
-  mu <-  Z%*%gamma + Xtheta%*%kappa
+  theta <- unname(lm(rep(1,nrow(B$psi))~B$psi-1)$coef)  # start with flat weight
+  theta <- theta*sqrt(nrow(B$psi)/sum(theta^2))  #scale starting weights
+  theta <- rep(theta,ngw)  # repeat it needed nuber of times  ( should have  ncol(B$psi)*ngw columns )
+  Xtheta <- matrix(0,n,ngb)  # weighted exposures ( should have ng columns )
+  for(g in 1:ng) Xtheta[Gb==g,ugb[g]] <- X[Gb==g,] %*% theta[(ugw[g]-1)*px+1:px]  #populate matrix
+  glmfit <- glm(Y~Xtheta+Z-1)  # starting values
+  kappa <- glmfit$coef[1:ngb]   # starting values for 
+  gamma <- glmfit$coef[-c(1:ngb)]  # coef for covariates
+  mu <-  Z%*%gamma + Xtheta%*%kappa  # predictor
 
 
   #place to store estiamtes
-  theta.keep <- matrix(NA,niter,px*ngw)
-  kappa.keep <- matrix(NA,niter,ngb)
-  gamma.keep <- matrix(NA,niter,pz)
-  res.keep <- matrix(NA,niter,n)
+  theta_keep <- matrix(NA,niter,px*ngw)
+  kappa_keep <- matrix(NA,niter,ngb)
+  gamma_keep <- matrix(NA,niter,pz)
+  res_keep <- matrix(NA,niter,n)
 
 
   pb <- txtProgressBar(min=0,max=niter, style=3, width=20)
@@ -90,7 +90,7 @@ bdlimglmall <- function(Y,X,Z,G,B,model,niter,nburn,nthin,prior,post){
     for(j in 1:nthin){
 
       #update gamma
-      yslice <- sum(post(Y,mu)) - sum(gamma^2)/2 + log(runif(1))
+      yslice <- -sum(family$dev.resids(Y,family$linkinv(mu),1))/2 - sum(gamma^2)/2 + log(runif(1))
       vgamma <-  rnorm(pz)*sqrt(prior$gamma)
       ang <- slicemax <- runif(1)*2*pi
       slicemin <- slicemax-2*pi
@@ -98,7 +98,7 @@ bdlimglmall <- function(Y,X,Z,G,B,model,niter,nburn,nthin,prior,post){
       while(notaccepted){
         gamma0 = gamma*cos(ang) + vgamma*sin(ang)
         mu0 <- mu - Z%*%gamma + Z%*%gamma0
-        if(sum(post(Y,mu0)) - sum(gamma0^2)/2  > yslice){
+        if(-sum(family$dev.resids(Y,family$linkinv(mu0),1))/2 - sum(gamma0^2)/2  > yslice){
           gamma <- gamma0
           mu <- mu0
           notaccepted <- FALSE
@@ -114,7 +114,7 @@ bdlimglmall <- function(Y,X,Z,G,B,model,niter,nburn,nthin,prior,post){
 
       #update theta (parameters for weight function)
       for(g in 1:ngw){
-        yslice <- sum(post(Y[Gw==g],mu[Gw==g])) - sum(theta[(ugw[g]-1)*px+1:px]^2)/2 + log(runif(1))
+        yslice <- -sum(family$dev.resids(Y[Gw==g],family$linkinv(mu[Gw==g]),1))/2 - sum(theta[(ugw[g]-1)*px+1:px]^2)/2 + log(runif(1))
         vtheta <- rnorm(px)
         vtheta <- vtheta*sqrt(nrow(B$psi)/sum(vtheta^2))
         ang <- slicemax <- runif(1)*2*pi
@@ -129,7 +129,7 @@ bdlimglmall <- function(Y,X,Z,G,B,model,niter,nburn,nthin,prior,post){
             for(gb in ugb[which(ugw==g)]) Xtheta0[Gb==gb & Gw==g,ugb[gb]] <- X[Gb==gb & Gw==g,] %*% theta0
 
             mu0[Gw==g] <- mu[Gw==g] - as.matrix(Xtheta[Gw==g,])%*%kappa + as.matrix(Xtheta0[Gw==g,])%*%kappa
-            if(sum(post(Y[Gw==g],mu0[Gw==g])) - sum(theta0^2)/2  > yslice){
+            if(-sum(family$dev.resids(Y[Gw==g],family$linkinv(mu[Gw==g]),1))/2 - sum(theta0^2)/2  > yslice){
               theta[(ugw[g]-1)*px+1:px] <- theta0
               mu[Gw==g] <- mu0[Gw==g]
               notaccepted <- FALSE
@@ -147,7 +147,7 @@ bdlimglmall <- function(Y,X,Z,G,B,model,niter,nburn,nthin,prior,post){
 
 
       #update kappa
-      yslice <- sum(post(Y,mu)) + log(runif(1))
+      yslice <- -sum(family$dev.resids(Y,family$linkinv(mu),1))/2 + log(runif(1))
       vkappa <-  rnorm(ngb)*sqrt(prior$beta)
       ang <- slicemax <- runif(1)*2*pi
       slicemin <- slicemax-2*pi
@@ -155,7 +155,7 @@ bdlimglmall <- function(Y,X,Z,G,B,model,niter,nburn,nthin,prior,post){
       while(notaccepted){
         kappa0 = kappa*cos(ang) + vkappa*sin(ang)
         mu0 <- mu - Xtheta%*%kappa + Xtheta%*%kappa0
-        if(sum(post(Y,mu0)) > yslice){
+        if(-sum(family$dev.resids(Y,family$linkinv(mu0),1))/2 > yslice){
           kappa <- kappa0
           mu <- mu0
           notaccepted <- FALSE
@@ -173,43 +173,47 @@ bdlimglmall <- function(Y,X,Z,G,B,model,niter,nburn,nthin,prior,post){
 
     }
 
-    theta.keep[i,] <- theta
-    gamma.keep[i,] <- gamma
-    kappa.keep[i,] <- kappa
-    res.keep[i,] <- mu
+    theta_keep[i,] <- theta
+    gamma_keep[i,] <- gamma
+    kappa_keep[i,] <- kappa
+    res_keep[i,] <- mu
   }
 
 
   #DIC for each observation. this should be done before the rescaling.
-  D_thetabar <- -2*post(Y,colMeans(res.keep[(nburn+1):niter,]))
+  D_thetabar <- family$dev.resids(Y,family$linkinv(colMeans(res_keep[(nburn+1):niter,])),1)
   Dbar <- rep(0,n)
-  for(i in 1:n) Dbar[i] <- Dbar[i] +  -2*mean(post(as.numeric(Y[i]),res.keep[(nburn+1):niter,i]))
+  for(i in 1:n) Dbar[i] <- Dbar[i] +  mean(family$dev.resids(rep(Y[i],niter-nburn),family$linkinv(res_keep[(nburn+1):niter,i]),1))
   pD <- Dbar - D_thetabar
   DIC <- pD+Dbar
 
-
+  # store thetas
   if(ngw>1){
     theta <- list()
     for(g in 1:ngw){
-      theta[[as.character(ug[g])]] <- theta.keep[(nburn+1):niter,(which(ug==ug[g])-1)*px+1:px]
+      theta[[as.character(ug[g])]] <- theta_keep[(nburn+1):niter,(which(ug==ug[g])-1)*px+1:px]
     }
   }else{
-    theta <- theta.keep[(nburn+1):niter,]
+    theta <- theta_keep[(nburn+1):niter,]
   }
-
-  if(ngb>1) colnames(kappa.keep) <- ug
-  colnames(gamma.keep) <- colnames(Z)
+  # format kappa_keep
+  if(ngb>1) colnames(kappa_keep) <- ug
+  colnames(gamma_keep) <- colnames(Z)
 
   #save output
-  out <- list(beta=kappa.keep[(nburn+1):niter,],
+  out <- list(beta=kappa_keep[(nburn+1):niter,],
               theta=theta,
-              gamma=gamma.keep[(nburn+1):niter,]
+              gamma=gamma_keep[(nburn+1):niter,]
   )
 
   #save DIC
   D <- D_thetabar
   out$DIC <- data.frame(G="Overall",DIC=sum(DIC),pD=sum(pD),Dbar=sum(Dbar),D=sum(D))
-  if(!is.null(G)) out$DIC <- rbind(out$DIC,aggregate(cbind(DIC,pD,Dbar,D), by=list(G=as.character(G)), sum))
+  if(!is.null(G)){
+    bysubgroup <- aggregate(cbind(DIC,pD,Dbar,D), by=list(G=as.character(G)), sum)
+    colnames(bysubgroup) <- colnames(out$DIC)
+  } 
+  out$DIC <- rbind(out$DIC,bysubgroup)
 
   return(out)
 }
