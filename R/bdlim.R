ff5cc8d2d9b62f7954307c00efba047128947a1f
Ander <ander.wilson@colostate.edu>
2017-08-09 11:52:33 -0600

    clean bdlim. add documentation, organize code, change where family and link are stored in output. No changes to model, starting values, or code.

--- R/bdlim.R
+++ R/bdlim.R
@@ -33,13 +33,14 @@ bdlim <- function(Y,X,Z,G=NULL,inter.model="all",family=gaussian,niter=1000,nbur
       family <- family()
   }
 
-
   #account for missing input in priors
   if(missing(prior)) prior <- NULL
-  if(is.null(prior$sigma)) prior$sigma <- c(0.001,0.001)
   if(is.null(prior$beta)) prior$beta <- Inf
   if(is.null(prior$gamma)) prior$gamma <- Inf
 
+  # make y, G vectors and X a matrix.
+  Y <- drop(Y)
+  G <- drop(G)
   X <- as.matrix(X)
   
   #deal with missing or null basis.opts
@@ -56,38 +57,39 @@ bdlim <- function(Y,X,Z,G=NULL,inter.model="all",family=gaussian,niter=1000,nbur
     stop("basis type not recognized.")
   }
   
-
-  #scaled data
+  #setup data
   x <- X%*%B$psi
-  # print(G)
   if(is.null(G)){
+    # add an overall intercept if there is no G
     z1 <- model.matrix(~Z)
     inter.model <- c("n")
   }else{
+    # group specific intercept if there is a G
     if(!is.factor(G)) G <- as.factor(G)
     z1 <- model.matrix(~G+Z-1)
   }
-  # print(head(z1))
+  # deal with multicolinearity
   z1 <- z1[,qr(z1)$pivot[1:qr(z1)$rank]]
   colnames(x) <- paste0("x",1:ncol(x))
 
-  # make y a vector
-  Y <- drop(Y)
-  G <- drop(G)
 
-  if(inter.model=="all"){
+  # which models to run.
+  if(any(toupper(inter.model)=="ALL")){
     runmods <- c("bw","b","w","n")
   }else{
     runmods <- inter.model
   }
-
   runmods <- c("BDLIM_n","BDLIM_bw","BDLIM_b","BDLIM_w")[which( c("N","BW","B","W") %in% toupper(runmods) )]
-
+  if(length(runmods)==0){
+    stop("no valid interaction model specified.  Muse be in: c('bw', 'b', 'w', 'n', 'all') ")
+  }
+  
+  # place to store results from one or more models
   fit <- list()
 
-  #fit normal linear model
+  # fit normal linear model
   if(family$family=="gaussian"){
-    if(is.null(prior$sigma)) prior$sigma <- c(1/2,0)
+    if(is.null(prior$sigma)) prior$sigma <- c(0.001,0.001)
 
     for(Gmodel in runmods){
       if(!missing(seed)) set.seed(seed)
@@ -119,14 +121,6 @@ bdlim <- function(Y,X,Z,G=NULL,inter.model="all",family=gaussian,niter=1000,nbur
       }
   }
 
-
-
-
-
-  fit[[Gmodel]]$inter.model <- Gmodel
-  fit[[Gmodel]]$family <- family$family
-  fit[[Gmodel]]$link <- family$link
-
   #summarize model fit
   modelfit <- NULL
   for(Gmodel in runmods){
@@ -139,18 +133,13 @@ bdlim <- function(Y,X,Z,G=NULL,inter.model="all",family=gaussian,niter=1000,nbur
   modelfit <- modelfit[,c("DIC","pD","modelprob")]
   fit$modelfit <- modelfit[order(-modelfit$modelprob),]
 
-
-
-  fit$family <- family
-
-
-
-
+  # summary information and other quantities of interest
+  fit$family <- family$family
+  fit$link <- family$link
   fit$B <- B
   fit$basis.opts <- basis.opts
   fit$prior <- prior
   fit$call <- match.call()
-
   class(fit) <- "bdlim"
 
   return(fit)
